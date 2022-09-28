03220f06f8528359e3bb207ec91effa337424e90
Damian192 <91610490+Damian192@users.noreply.github.com>
2022-09-28 09:09:29 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,28 @@
+/*  TYPY ZMIENNYCH
+mozliwe sa nastepujace typy danych:
+typ liczbowy: number
+typ łańcuchowy: string
+typ logiczny: boolean
+typ null
+typ undefined
+*/
+
+//typ number
+var a = 12;
+var b = 837; // osemkowy
+var c = 0xACB; // szesnastkowy
+var d = 0.12e-2; // notacja wykladnicza
+
+//typ string
+var e = "warszawa";
+var f = 'krakow';
+
+//typ boolean
+var g = true;
+var h = false;
+
+//typ null nie przechowuje zadnej watosci.
+// w javascript jest objektem
+
+//typ undefined 
+var x;
