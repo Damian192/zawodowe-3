dbe8a7a0f7dcf2561a1a83516979dc24e6ecb943
Damian192 <91610490+Damian192@users.noreply.github.com>
2022-10-07 14:05:25 +0200

    Create java.java

---
+++ java.java
@@ -0,0 +1,26 @@
+package test;
+
+
+public class Testowa_klasa {
+	
+	public static void main(string[] args) {
+		int x=5;
+		System.out.print("Zmienna wynosi :" + x);
+			char znak = "z";
+		System.out.print("Zmienna wynosi :" + znak);
+			float i = 451f;
+		System.out.print("Zmienna wynosi :" + i);
+			double y = 2.55;
+		System.out.print("Zmienna wynosi :" + y);
+			string s = "JAVA";
+		System.out.print("Zmienna wynosi :" + s);
+		
+		char[] tab
+		// w java string jest traktowany jako klasa
+		
+		String tab_s = newString(tab);
+		
+		System.out.printIn("zmienna tab_s wynosi:" + tab_s);
+		
+	}
+}
