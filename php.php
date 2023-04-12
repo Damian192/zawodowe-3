ebfcbd273d982fdcc6337dee43cf948dd81a4fb3
Damian192 <91610490+Damian192@users.noreply.github.com>
2023-04-12 11:06:30 +0200

    Update php.php

--- php.php
+++ php.php
@@ -203,4 +203,30 @@ switch($kolor) {
     <input type="submit" name="button2" value="pokaz"/>
 </form>
 </p>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <p>
+    
+<?php
+   $x;
+   for($x = 150;$x >= 100;$x--)
+   if($x%5==0) {
+    echo $x."<br>";
+   }
+?>
+</p>
+</body>
+</html>
         
